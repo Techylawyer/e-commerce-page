6ec3b525edc45e25d575cb470e64bb35e9c6e1fd
Techylawyer <oluwasegungrace1@gmail.com>
2025-08-21 16:48:01 +0100

    add functionality to scroll product images on mobile

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,9 +1,31 @@
+import { useState } from 'react'
+
+const productImages = [
+  '/images/image-product-1.jpg',
+  '/images/image-product-2.jpg',
+  '/images/image-product-3.jpg',
+  '/images/image-product-4.jpg',
+]
+
 function Product() {
+  const [currentImage, setCurrentImage] = useState(0)
+
+  const previousImage = () => {
+    setCurrentImage((prev) =>
+      prev === 0 ? productImages.length - 1 : prev - 1
+    )
+  }
+
+  const nextImage = () =>  {
+    setCurrentImage(prev => prev === productImages.length -1 ? 0 : prev + 1)
+  }
+
+
   return (
     <section className="product relative w-full max-w-md mx-auto">
       <img
-        src="images/image-product-1.jpg"
-        alt="product image"
+        src={productImages[currentImage]}
+        alt={`product image ${currentImage + 1}`}
         className="w-screen md:w-full md:h-full md:rounded-lg"
       />
 
@@ -12,6 +34,7 @@ function Product() {
           src="images/icon-previous.svg"
           alt="previous"
           className="block w-2.5 h-2.5"
+          onClick={previousImage}
         />
       </button>
 
@@ -20,9 +43,12 @@ function Product() {
           src="images/icon-next.svg"
           alt="next"
           className="block w-2.5 h-2.5"
+          onClick={nextImage}
         />
       </button>
     </section>
   )
 }
+
+
 export default Product
